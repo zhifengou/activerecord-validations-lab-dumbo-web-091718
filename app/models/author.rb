332f4a6c171e2c15e2d92a05d34c67f067642624
zhifeng ou <zhifengou@hotmail.com>
2018-10-15 20:28:04 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :phone_number, length: {minimum: 10}
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum:250}
+  validates :summary, length: {maximum:250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait_validator
+  
+def clickbait_validator
+    if [/Won't Believe/, /Secret/, /Top [0-9]*/, /Guess/].none? {|clickbait| clickbait.match(title)}
+      errors.add(:title, "must be clickbait")
+  end
+end	
+
 end
